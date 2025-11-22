6617b2a4dcc03e1e8d46306b8d5f6538c867cced
sonicknucklesss <pravin.bp.art@gmail.com>
2025-11-22 22:03:29 +0530

    init

--- preload.js
+++
@@ -1,6 +0,0 @@
-// preload
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("slab", {
-  onDisplaysChanged: cb => ipcRenderer.on("displays-changed", (_, d) => cb(d))
-});
